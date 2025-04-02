82340a3e28a631968565122a39249485785b768d
clowesait <cody.lowe@edu.sait.ca>
2025-04-01 20:01:03 -0600

    Created Skeleton Pages

---
+++ app/contact/page.js
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+      <main className="flex min-h-screen flex-col items-center">
+        <h1>Contact Page</h1>
+      </main>
+    );
+  }
---
+++ app/gallery/page.js
@@ -0,0 +1,7 @@
+export default function Gallery() {
+    return (
+      <main className="flex min-h-screen flex-col items-center">
+        <h1>Gallery Page</h1>
+      </main>
+    );
+  }
